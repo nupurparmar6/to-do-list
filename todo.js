d2592e50ad3a4fdf038c1a7d9cf36bad935629f0
“nupurparmar6” <nupurparmar6@gmail.com>
2020-07-28 04:03:52 +0530

    add functionality

--- todo.js
+++ todo.js
@@ -1,1 +1,29 @@
 
+//adding a strike through effect when the event is clicked ie just completed
+$("ul").on("click","li",function(){
+    $(this).toggleClass("strike");
+});
+
+//deleting an element
+$("ul").on("click",".delete",function(){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+//converting text input to an element (li) in the list
+$("input[type='text']").on("keypress",function(event){
+
+    if(event.which===13){
+        //if to do is not empty
+        if(true){
+            var content= $(this).val();
+            $("ul").append("<li><span class=\"delete\"><i class=\"fas fa-trash\"></i></span>"+content+"</li>");
+        }
+        $(this).val("");
+
+    }
+
+
+});
